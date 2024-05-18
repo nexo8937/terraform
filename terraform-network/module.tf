3fd63267ae1c3045ec88509623b44a4aa4240768
Yeghishe <85505705+nexo8937@users.noreply.github.com>
2024-05-18 13:53:08 +0400

    Update module.tf

--- terraform-network/module.tf
+++ terraform-network/module.tf
@@ -4,4 +4,5 @@ module "network" {
     vpc_cidr_block        = "10.0.0.0/16"
     public_subnet_ciders  = ["10.0.11.0/24", "10.0.12.0/24"]    
     private_subnet_ciders = ["10.0.21.0/24", "10.0.22.0/24"]
+    db_subnet_ciders      = ["10.0.31.0/24", "10.0.32.0/24"]
  }
